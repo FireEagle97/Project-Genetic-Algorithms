7975bb0172a7945b9d393ddad47ce378fc18b4e5
Bastian Fernandez Cortez <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-10-24 12:13:14 -0400

    Amended game class name in Program.cs

--- RobbyVisualizer/Program.cs
+++ RobbyVisualizer/Program.cs
@@ -7,7 +7,7 @@ namespace RobbyVisualizer
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new RobbyVisualizerGame())
                 game.Run();
         }
     }
