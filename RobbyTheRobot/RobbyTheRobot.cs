4c7a583b6e33d1f9d14b7169ff29629893236186
Bastian Fernandez Cortez <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-10-24 12:57:55 -0400

    Started Robby code

--- RobbyTheRobot/RobbyTheRobot.cs
+++ RobbyTheRobot/RobbyTheRobot.cs
@@ -2,7 +2,35 @@
 
 namespace RobbyTheRobot
 {
-    public class RobbyTheRobot
+    public class RobbyTheRobot : IRobbyTheRobot
     {
+        public int NumberOfActions {get;}
+
+        public int NumberOfTestGrids {get;}
+
+        public int GridSize {get;}
+
+        public int NumberOfGenerations {get;}
+
+        public double MutationRate {get;}
+
+        public double EliteRate {get;}
+
+        public ContentsOfGrid[,] GenerateRandomTestGrid()
+        {
+            int gridSize = GridSize*GridSize;
+            int gridHalf = GridSize/2;
+            ContentsOfGrid[,] grid = new ContentsOfGrid[GridSize, GridSize];
+            Random random = new Random();
+            int canNumber = random.Next();
+            return grid;
+        }
+
+        public void GeneratePossibleSolutions(string folderPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        
     }
 }
