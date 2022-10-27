a912b3a9d865c44a2d7c9a04a1a1969913330615
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2022-10-27 10:33:08 -0400

    add FillChromosome()

--- GeneticAlgorithm/Chromosome.cs
+++ GeneticAlgorithm/Chromosome.cs
@@ -1,15 +1,21 @@
+using System;
 namespace GeneticAlgorithm{
 
     public class Chromosome : IChromosome{
-        Chromosome(double fitness, int[] genes){
-            Fitness = fitness;
-            Genes = genes;
+        private int[] genes;
+        private double fitness;
+        Chromosome(){
+            genes = new int[243];
         }
         public double Fitness{
-            get;
+            get{
+                return this.fitness;
+            }
         }
         public int[] Genes{
-            get;
+            get{
+                return this.genes;
+            }
         }
         public long Length{
             get{
@@ -22,7 +28,16 @@ namespace GeneticAlgorithm{
 
             }
         }
+        public int[] FillChromsome(){
+            Random rnd =new Random();
+            for(var i =0; i < Genes.Length; i++){
+                var randNum = rnd.Next(0,6);
+                Genes[i] = randNum;
+            }
+            return this.Genes;
+        }
         public IChromosome[] Reproduce(IChromosome spouse, double mutationRate){
+            
             
         }
 
