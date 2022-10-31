b1b7c98033427202a15675a2a10ee9236b27d155
Bastian Fernandez Cortez <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-10-31 12:57:47 -0400

    Modified the constructor of Generation

--- GeneticAlgorithm/Generation.cs
+++ GeneticAlgorithm/Generation.cs
@@ -2,31 +2,7 @@ namespace GeneticAlgorithm{
     
     internal class Generation : IGenerationDetails
     {
-        //public delegate double FitnessEventHandler(IChromosome chromosome, IGeneration generation);
-
         private IChromosome[] _chromosomeArray;
-
-
-
-        // One that takes the IGeneticAlgorithm, FitnessEventHandler, and a potential seed
-        public Generation(IGeneticAlgorithm geneticAlgorithm, FitnessEventHandler fitnessCalculation, int? seed = null)
-        {
-            var random = seed.HasValue ? new Random(seed.Value) : new Random();
-            _chromosomeArray = new IChromosome[geneticAlgorithm.PopulationSize];
-            for (var i = 0; i < geneticAlgorithm.PopulationSize; i++)
-            {
-                _chromosomeArray[i] = new Chromosome(geneticAlgorithm, random);
-            }
-        }
-        //  Performs a deep copy the generation based on an array of IChromosomes
-        public Generation(IChromosome[] chromosomeArray)
-        {
-            _chromosomeArray = new IChromosome[chromosomeArray.Length];
-            for (var i = 0; i < chromosomeArray.Length; i++)
-            {
-                _chromosomeArray[i] = chromosomeArray[i].Clone();
-            }
-        }
         public IChromosome this[int index] { get{
             return _chromosomeArray[index];
             }
@@ -38,12 +14,28 @@ namespace GeneticAlgorithm{
 
         public long NumberOfChromosomes { get; }
 
-        public Generation(IGeneticAlgorithm geneticAlgorithm, FitnessEventHandler fitnessEventHandler, int? seed = null){
-            //deep copy?
+        // One that takes the IGeneticAlgorithm, FitnessEventHandler, and a potential seed
+        public Generation(IGeneticAlgorithm geneticAlgorithm, FitnessEventHandler fitnessCalculation, int? seed = null)
+        {
+            _chromosomeArray = new IChromosome[geneticAlgorithm.PopulationSize];
+            for (var i = 0; i < geneticAlgorithm.PopulationSize; i++)
+            {
+                _chromosomeArray[i] = new Chromosome(geneticAlgorithm.NumberOfGenes, geneticAlgorithm.LengthOfGene, seed);
+            }
+        }
+        //  Performs a deep copy the generation based on an array of IChromosomes
+        public Generation(IChromosome[] chromosomeArray)
+        {
+            _chromosomeArray = new IChromosome[chromosomeArray.Length];
+            for (var i = 0; i < chromosomeArray.Length; i++)
+            {
+                _chromosomeArray[i] = chromosomeArray[i].Clone();
+            }
         }
 
         public void EvaluateFitnessOfPopulation()
         {
+            fitnessCalculation?.Invoke();
             foreach(IChromosome element in _chromosomeArray){
                 GeneticAlgorithm.FitnessCalculation(element, this);
             }
