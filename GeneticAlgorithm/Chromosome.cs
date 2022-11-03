6628437c4a4be4c104e7c68995b41116aee934c0
Harout Dabbaghian <haroutdabagh@gmail.com>
2022-11-03 11:36:40 -0400

    refactored the Chromosome.cs

--- GeneticAlgorithm/Chromosome.cs
+++ GeneticAlgorithm/Chromosome.cs
@@ -1,113 +1,167 @@
 using System;
-namespace GeneticAlgorithm{
+namespace GeneticAlgorithm
+{
 
-    public class Chromosome : IChromosome{
+    public class Chromosome : IChromosome
+    {
         private int[] _genes;
         private double _fitness;
         private Random _rnd;
-        Chromosome(int numOfGenes, int potSeed){
-            NumOfGenes = numOfGenes;
-            PotSeed = potSeed;
-            _genes = new int[NumOfGenes];
-            _rnd = new Random(PotSeed);
-            Fitness = _fitness;
+
+        //One that takes the number of genes, the length of a gene, and a potential seed
+        public Chromosome(int numberOfGenes, int? seed = null)
+        {
+            _rnd = seed.HasValue ? new Random(seed.Value) : new Random();
+            _genes = new int[numberOfGenes];
+            for (int i = 0; i < numberOfGenes; i++)
+            {
+                _genes[i] = _rnd.Next(0, 6);
+            }
 
         }
-        Chromosome(){
-            Chromosome duplicated = new Chromosome(this.NumOfGenes, this.PotSeed);
-        }
-        public double Fitness{
-            get{
-                return this._fitness;
+        //Performs a deep copy of the Chromosome
+        public Chromosome(Chromosome other)
+        {
+            _genes = new int[other._genes.Length];
+            for (int i = 0; i < other._genes.Length; i++)
+            {
+                _genes[i] = other._genes[i];
             }
-            set{
-                this._fitness =value;
-            }
+            _fitness = other._fitness;
         }
-        public int NumOfGenes{
-            get;
-        }
-        public int PotSeed{
-            get;
-        }
-        public int[] Genes{
-            get{
-                return this._genes;
-            }
-        }
-        public long Length{
-            get{
-                return Genes.Length;
-            }
-        }
-        public int this[int index]{
-            get{
+
+        /// <summary>
+        /// The fitness score of the IChromosome
+        /// </summary>
+        /// <value>A value representing the fitness of the IChromosome</value>
+        public double Fitness => _fitness;
+
+        /// <summary>
+        /// The length of the genes
+        /// </summary>
+        
+        public long Length => _genes.Length;
+
+        
+        // int[] Genes { get; }
+        public int[] Genes => _genes;
+
+
+        /// <summary>
+        /// Returns the current gene at the provided position
+        /// </summary>
+        /// <value></value>
+
+        public int this[int index]
+        {
+            //use Genes
+            get
+            {
                 return Genes[index];
-
             }
-            set{
+            set
+            {
                 Genes[index] = value;
             }
+        }
 
-        }
-        public int[] FillChromsome(){
-            for(var i =0; i < Genes.Length; i++){
-                var randNum = _rnd.Next(0,6);
-                Genes[i] = randNum;
-            }
-            return this.Genes;
-        }
-        private Chromosome mutate(Chromosome child, double mutationRate){
-            //ex mutationRate 0.2
-            double[] mutationArr = new double[100];
-            for(var i =0;i<mutationArr.Length;i++){
-                mutationArr[i]= _rnd.NextDouble();
-            }
-            for(var j=0; j< child.Length;j++){
-                if(mutationRate < mutationArr[j]){
-                    child[j] = _rnd.Next(0,6);
+
+        /// <summary>
+        /// muation function
+        /// </summary>
+        private Chromosome mutate(Chromosome child, double mutationRate)
+        {
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                if (_rnd.NextDouble() < mutationRate)
+                {
+                    child[i] = _rnd.Next(0, 6);
                 }
             }
             return child;
         }
-        public IChromosome[] Reproduce(IChromosome spouse, double mutationRate){
-            var lowerBound = _rnd.Next(0,121);
-            var upperBound = _rnd.Next(121,243);
-            if (mutationRate > 1 || mutationRate < 0){
-                throw new ArgumentException("Mutation rate should me between 0 and 1");
+
+
+        /// <summary>
+        /// Uses a crossover function to create two offspring, then iterates through the
+        /// two child Chromosomes genes, changing them to random values according to the mutation rate.
+        /// </summary>
+        /// <param name="spouse">The Chromosome to reproduce with</param>
+        /// <param name="mutationProb">The rate of mutation</param>
+        /// <returns></returns>
+
+        //this is the better way test both
+        public IChromosome[] Reproduce(IChromosome spouse, double mutationProb)
+        {
+            Chromosome[] children = new Chromosome[2];
+            children[0] = new Chromosome(this);
+            children[1] = new Chromosome((Chromosome)spouse);
+            int splitPoint = _rnd.Next(0, _genes.Length);
+            for (int i = splitPoint; i < _genes.Length; i++)
+            {
+                children[0][i] = spouse[i];
+                children[1][i] = this[i];
+                // children[0].Genes[i] = spouse[i];
+                // children[1].Genes[i] = this[i];
             }
-            Chromosome[] childChromosomes = new Chromosome[2]; 
-            Chromosome child1 = new Chromosome(243,5);
-            Chromosome child2 = new Chromosome(243,4);
-            for(var i=0; i<spouse.Length;i++){
-                if(i >= lowerBound && i <= upperBound){
-                    child1[i]=this.Genes[i];
-                }
-                child1[i]= spouse[i];
-            }
-            for(var j=0; j<this.Genes.Length;j++){
-                if(j>= lowerBound && j<= upperBound){
-                    child2[j]= spouse[j];
-                }
-                child2[j] = this.Genes[j];
-            }
-        
-            childChromosomes[0]= this.mutate(child1,mutationRate);
-            childChromosomes[1]=this.mutate(child2,mutationRate);
-            return childChromosomes;     
+            children[0] = mutate(children[0], mutationProb);
+            children[1] = mutate(children[1], mutationProb);
+            return children;
         }
 
-        public int CompareTo(IChromosome obj){
-            if(obj == null){
+        //test with this too
+        //     Chromosome child1 = new Chromosome(this.NumOfGenes, this.PotSeed);
+        //     Chromosome child2 = new Chromosome(this.NumOfGenes, this.PotSeed);
+        //     int[] parent1Genes = this.Genes;
+        //     int[] parent2Genes = spouse.Genes;
+        //     int crossoverPoint = _rnd.Next(0, this.NumOfGenes);
+        //     for(var i = 0; i < crossoverPoint; i++){
+        //         child1.Genes[i] = parent1Genes[i];
+        //         child2.Genes[i] = parent2Genes[i];
+        //     }
+        //     for(var i = crossoverPoint; i < this.NumOfGenes; i++){
+        //         child1.Genes[i] = parent2Genes[i];
+        //         child2.Genes[i] = parent1Genes[i];
+        //     }
+        //     children[0] = mutate(child1, mutationProb);
+        //     children[1] = mutate(child2, mutationProb);
+        //     return children;
+
+        // }
+        //Chromosomes should be compared based on their Fitness
+        //Test both these functions
+        public int CompareTo(IChromosome other)
+        {
+            if (other == null)
+            {
                 return 1;
             }
-            Chromosome otherChromosome = obj as Chromosome;
-            if (otherChromosome != null)
-                return this.Fitness.CompareTo(otherChromosome.Fitness);
-            else    
-                throw new ArgumentException("object is not a Chromosome");    
-
+            if (this.Fitness > other.Fitness)
+            {
+                return 1;
+            }
+            else if (this.Fitness < other.Fitness)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
         }
+        // public int CompareTo(IChromosome obj)
+        // {
+        //     if (obj == null)
+        //     {
+        //         return 1;
+        //     }
+        //     Chromosome otherChromosome = obj as Chromosome;
+        //     if (otherChromosome != null)
+        //         return this.Fitness.CompareTo(otherChromosome.Fitness);
+        //     else
+        //         throw new ArgumentException("object is not a Chromosome");
+
+        // }
 
     }
 }
