2c1e12ac083995b7e37788bf07ee1e4d49dc89ab
BastianFC0 <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-11-03 19:19:06 -0400

    Finished grid method, now figuring out GeneratePossibleSolutions

--- RobbyTheRobot/RobbyTheRobot.cs
+++ RobbyTheRobot/RobbyTheRobot.cs
@@ -6,26 +6,26 @@ namespace RobbyTheRobot
     //delegate for notifying when a file has been written
     internal class RobbyTheRobot : IRobbyTheRobot
     {
-        public int NumberOfActions {get;}
-
-        public int NumberOfTestGrids {get;}
-
+        public int NumberOfActions {get; set;}
+        public int NumberOfTestGrids {get; set;}
         public int GridSize {get;}
-
         public int NumberOfGenerations {get;}
-
         public double MutationRate {get;}
-        public WriteFileHandler WriteFile {get;}
+        public event WriteFileHandler FileWritten;
         public double EliteRate {get;}
         //Unsure if should exist
         public int PopulationSize {get;}
         //Unsure if should exist
         public int NumberOfTrials {get;}
+
         public RobbyTheRobot(int numberOfGenerations, int populationSize, int numberOfTrials, int? seed = null){
             NumberOfGenerations = numberOfGenerations;
             PopulationSize = populationSize;
             NumberOfTrials = numberOfTrials;
-            //set GridSize
+            //Instructions stipulate that the size of the grid is 100
+            GridSize = 100;
+            //and Robby can do 200 possible actions
+            NumberOfActions = 200;
         }
 
         public ContentsOfGrid[,] GenerateRandomTestGrid()
@@ -38,7 +38,7 @@ namespace RobbyTheRobot
                 ContentsOfGrid[,] grid = new ContentsOfGrid[rowSize, rowSize];
                 Random random = new Random();
                 int gridHalf = GridSize/2;
-                List<int> canCheckerList = new List<int>;
+                List<int> canCheckerList = new List<int>();
 
                 //Populate it with cans
                 for(int i = 0; i < gridHalf; i++){
@@ -49,14 +49,15 @@ namespace RobbyTheRobot
                         canNumber = random.Next(0, GridSize);
                     }
                     //int to use to determine the position in the outer array
-                    int canPositionOuter = canNumber;
+                    int canPositionOuter;
                     //int to use to determine the position in the inner array
                     int canPositionInner = canNumber;
                     
                     //Determine outer can position
+                    canPositionOuter = Convert.ToInt32(Math.Floor((double)canNumber/rowSize));
                     //Determine inner can position
-                    while(canPositionInner > rowSize){
-                        canPositionInner -= rowSize;
+                    while(canPositionInner > rowSize-1){
+                        canPositionInner -= rowSize-1;
                     }
 
                     //Insert can in grid
@@ -72,9 +73,47 @@ namespace RobbyTheRobot
 
         public void GeneratePossibleSolutions(string folderPath)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < NumberOfTrials; i++){
+                //Create the grid
+                ContentsOfGrid[,] grid = GenerateRandomTestGrid();
+                for(int j = 0; j < NumberOfGenerations; j++){
+                    //Scores for all chromosomes
+                    int[] arrayOfScores = new int[PopulationSize];
+                    for(int k = 0; k < PopulationSize; k++){
+                        //Variable to hold the score
+                        int score = 0;
+                        for(int l = 0; l < NumberOfActions; l++){
+                            //Ends the scoring if all cans are found. I don't know if this is nesessary
+                            bool endScoringCheck = true;
+                            foreach(var content in grid){
+                                if(content == ContentsOfGrid.Can){
+                                    endScoringCheck = false;
+                                }
+                            }
+                            if(endScoringCheck){
+                                //Breaks this loop, ending the scoring
+                                break;
+                            }
+                            score += RobbyHelper.ScoreForAllele(, grid, , , );
+                        }
+                        arrayOfScores[k] = score;
+                    }
+                    //Save certain candidates if on gen 1, 20, 100, 200, 500, 1000
+                    if(j == 0 || j == 19 || j == 99 || j == 119 || j == 499 || j == 999){
+                        //Put data in file or prepare it in a comma separated list like so:
+                        //max score, number of moves to display, all moves
+
+                        //Loop to find max score in arrayOfScores
+                        //number of moves to display ???
+                        //all moves is all genes of a Chromosome
+                    }
+                    //Create next gen???
+                }
+                //Reset the gen for next trial?
+                NumberOfTestGrids += 1; //Unsure if it goes here
+            }   
+            //Write/Save to file 😖
         }
 
-        
     }
 }
