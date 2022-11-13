b9d02051a5bf916cb489b82e695da73bb8e7f32e
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2022-11-13 15:41:46 -0500

    add chromosome test case

--- GeneticAlgorithmTests/GenerationTests.cs
+++ GeneticAlgorithmTests/GenerationTests.cs
@@ -27,6 +27,30 @@ public class GenerationTests
         Assert.AreEqual(generation, geneticAlgorithm.CurrentGeneration);
 
     }
+    //Test Generation Chromosome Array
+    [TestMethod]
+    public void TestChromosomeArray()
+    {
+         //Arrange
+        int populationSize = 10;
+        int numberOfGenes = 10;
+        int lengthOfGene = 6;
+        double mutationRate = 0.1;
+        double eliteRate = 0.1;
+        int numberOfTrials = 10;
+        int seed = 4;
+        FitnessEventHandler? fitnessEventHandler = null;
+        GeneticAlgorithm.GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm.GeneticAlgorithm(populationSize, numberOfGenes, lengthOfGene, mutationRate, eliteRate, numberOfTrials, fitnessEventHandler, seed);
+        Chromosome[] chromsomesArr = new Chromosome[geneticAlgorithm.PopulationSize];
+        for (int i = 0; i < geneticAlgorithm.PopulationSize; i++)
+            {
+                chromsomesArr[i] = new Chromosome(geneticAlgorithm.NumberOfGenes, geneticAlgorithm.LengthOfGene, seed);
+            }  
+        //Act
+        Generation generationTest = new Generation(geneticAlgorithm, fitnessEventHandler,seed);
+        //Assert
+        Assert.AreEqual(chromsomesArr, generationTest.ChromosomesArray);
+    }
 
 }
 }
