1d62fe6e5371a452f2146490a8ce183d08d2bc93
Harout Dabbaghian <haroutdabagh@gmail.com>
2022-11-18 00:05:11 -0500

    add comments in console program

--- RobbyIterationGenerator/Program.cs
+++ RobbyIterationGenerator/Program.cs
@@ -18,7 +18,6 @@ namespace RobbyIterationGenerator
 
 
             Console.WriteLine("What is the population size? (recommended 200)");
-            //if user input is a letter, it will throw an exception
             int populationSize = Convert.ToInt32(Console.ReadLine());
             TakeUserInput(ref populationSize);
 
@@ -28,11 +27,8 @@ namespace RobbyIterationGenerator
             TakeUserInput(ref numberOfTrials);
 
 
-
-            //Create a new instance of Robby (has seed for testing)
             IRobbyTheRobot robby = Robby.CreateRobbyTheRobot(numberOfGenerations, populationSize, numberOfTrials);
 
-            //prompts the user where to save the text file
             Console.WriteLine("Where do you want to save the text file?");
             string path = Console.ReadLine();
             Console.WriteLine("Press x if you wish to stop the execution of the program");
@@ -68,6 +64,9 @@ namespace RobbyIterationGenerator
             Console.WriteLine("Generation took {0} milliseconds", watch.ElapsedMilliseconds);
         }
 
+        ///<summary>
+        ///Takes user input and checks if it is a valid number
+        ///</summary>
         public static void TakeUserInput(ref int input)
         {
             bool valid = false;
